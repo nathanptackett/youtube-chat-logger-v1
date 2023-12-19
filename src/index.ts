eeb3bdf564c7970da0fae91fb2f5d6c7c6707fb1
Nathan Tackett <nathan.tackett@veraxx.com>
2023-12-19 12:29:50 -0500

    Add the ability to pass in a broadcast id
    Add the ability to pass in a broadcast id. This
    is the broadcast id of the stream to log the
    chat from.

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,57 @@
-import YouTubeAPI from './YouTubeAPI'; // Adjust the path as needed
+import YouTubeAPI from './YouTubeAPI';
+import yargs from 'yargs';
+import { hideBin } from 'yargs/helpers'
 import 'dotenv/config';
 
 async function main() {
+    const argv = await yargs(hideBin(process.argv))
+        .option('broadcast_id', {
+            alias: 'b',
+            describe: 'The broadcast ID',
+            type: 'string'
+        })
+        .option('h', {
+            alias: 'help',
+            describe: 'Print help',
+            type: 'boolean'
+        })
+        .argv;
+
+    if (argv.help) {
+        yargs.showHelp();
+        return;
+    }
+
+    if (!argv.broadcast_id) {
+        console.log('Please provide a broadcast ID to operate on via -b,--broadcast-id <broadcast id>')
+        return;
+    }
+
     const apiKey = process.env.YOUTUBE_API_KEY;
     if (!apiKey) {
         throw new Error("YouTube API key is not set in .env file.");
     }
 
     const youtube = new YouTubeAPI(apiKey);
-    const channelName = await youtube.getChannelName('UC_5niPa-d35gg88HaS7RrIw');
-    console.log('Channel Name:', channelName);
+    const videoInfo = await youtube.getVideoInfo(argv.broadcast_id)
+    if(!videoInfo) {
+        console.error("Failed to access video with id of ", argv.broadcast_id)
+        return
+    }
+
+    const title = videoInfo.title;
+    const channelName = videoInfo.channelTitle;
+    const isLiveBroadcast = videoInfo.liveBroadcastContent == 'live' || videoInfo.liveBroadcastContent == 'upcoming';
+    console.log(videoInfo.liveBroadcastContent)
+
+    console.log("Succesfully opened video \"" + title + "\" by \"" + channelName + "\"")
+    if(!isLiveBroadcast) {
+        console.error("Error: This application only operates on live videos")
+        return
+    }
+
+    // TODO - Add chat logging
+    console.log("Logging chat from video \"" + title + "\"");
 }
 
 main();
